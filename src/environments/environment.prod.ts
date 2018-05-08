ea1d0c777ecd362fa614c9669484087fdea1d07a
Bartnik, Szymon <szymon.bartnik@krollontrack.com>
2018-05-08 14:46:02 +0200

    Fixed travis #3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   notificationsPublicKey: 'BAvDMcexg3uaneVs-6pFGMVFw2PPUkIKEtgj5N4t-bB3Fcednt6kN3qQ4tMy3vNBkZmgxnKhQA5Y6sueaRJyehE',
-  subscribeEndpoint: 'http://localhost:5000/pwa-angular-campus/us-central1/subscribeApi',
+  subscribeEndpoint: 'https://us-central1-pwa-angular-campus.cloudfunctions.net/subscribeApi',
   firebase: {
     apiKey: 'AIzaSyCXRL9U96v8i404SpGkG0Wivi9J0FrXcg8',
     authDomain: 'pwa-angular-campus.firebaseapp.com',
