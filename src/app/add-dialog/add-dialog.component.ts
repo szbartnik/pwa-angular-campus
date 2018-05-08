b9e3136a2e957af9978c6a968dbf5eb9c80d9110
ALTEGRITY\lukasz.godula <lukasz.godula@krollontrack.com>
2018-05-08 15:10:11 +0200

    snack bar

--- src/app/add-dialog/add-dialog.component.ts
+++ src/app/add-dialog/add-dialog.component.ts
@@ -1,5 +1,7 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Observable } from 'rxjs/Observable';
+
 import { PersistenceService } from '../core/persistence.service';
 
 @Component({
@@ -7,18 +9,27 @@ import { PersistenceService } from '../core/persistence.service';
   templateUrl: './add-dialog.component.html',
   styleUrls: ['./add-dialog.component.css']
 })
-export class AddDialogComponent {
+export class AddDialogComponent implements OnInit {
 
   public isFinalizeDisabled = true;
   public recordName: string;
   public recordNotes: string;
   public downloadUrl: string;
+  public isConnected: Observable<boolean>;
 
   constructor(
     public dialogRef: MatDialogRef<AddDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private persistenceService: PersistenceService
-  ) { }
+  ) {
+    this.isConnected = Observable.merge(
+      Observable.of(navigator.onLine),
+      Observable.fromEvent(window, 'online').map(() => true),
+      Observable.fromEvent(window, 'offline').map(() => false));
+   }
+
+   ngOnInit() {
+   }
 
   public onNoClick(): void {
     this.dialogRef.close();
